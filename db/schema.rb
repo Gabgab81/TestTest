be92bb6be3e15f541015a8df2010dd275e82460c
gabriel roux <rouxgab81@gmail.com>
2023-04-21 14:40:20 -0400

    remove skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_183229) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_131926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,14 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_183229) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +35,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_183229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "skills", "users"
 end
